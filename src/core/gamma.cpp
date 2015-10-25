5d2db27287cf70d99f615abefae98e245a534656
Francesco Abbate <francesco.bbt@gmail.com>
2015-10-26 00:33:56 +0100

    Update old commented code for information purpose

--- src/core/gamma.cpp
+++ src/core/gamma.cpp
@@ -5,7 +5,7 @@ gamma_type sys_gamma(1.5);
 #endif
 // Original values for the subpixel color filter. These are quite conservative
 // to avoid color fringes.
-// agg::lcd_distribution_lut gslshell::subpixel_lut(3./9., 2./9., 1./9.);
+// agg::lcd_distribution_lut subpixel_lut(3./9., 2./9., 1./9.);
 
 // Slightly more agressive values with more weight to primary channel.
 // Some people may notice colorer fringes but fonts looks sharper.
